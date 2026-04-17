aed398883da77f27fdb47bd836a63c3e97f57e8f
Norahxj <Nhaljuhani@gmail.com>
2026-04-17 13:39:46 +0000

    Coroutine that runs multiple asyncio tasks, Task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,11 +8,11 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn task_wait_random n times and return the delays in ascending order."""
+    """Return the list of delays in ascending order."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
-    for task in asyncio.as_completed(
-            [task_wait_random(max_delay) for _ in range(n)]):
+    for task in asyncio.as_completed(tasks):
         delays.append(await task)
 
     return delays
