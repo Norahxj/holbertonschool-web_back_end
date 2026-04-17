b37bb8076e8d673093a5c094f7090b0379fdfb98
Norahxj <Nhaljuhani@gmail.com>
2026-04-17 13:45:01 +0000

    Defines an asynchronous generator, Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous generator."""
+"""Module that defines the async_generator coroutine."""
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random numbers between 0 and 10 asynchronously."""
+async def async_generator() -> Generator[float, None, None]:
+    """Yield 10 random numbers between 0 and 10 with a 1-second delay."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
