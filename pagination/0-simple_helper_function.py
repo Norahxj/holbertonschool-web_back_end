03069de3ba3e2ebbc6593c6e7e0d98363bf9dcd3
Norahxj <Nhaljuhani@gmail.com>
2026-04-29 17:31:51 +0000

    Added helper function, Task 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module provides a helper function for pagination indexes."""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return the start and end indexes for the given pagination page."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
